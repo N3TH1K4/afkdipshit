b294c82e7df667e7eab1982939d8a74f83de849f
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-08-15 16:18:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = 18983092
-API_HASH ="a6005a70e88369b4fb08b8350ebbdd35"
+API_ID = 18983
+API_HASH ="a6005a7
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = "5586017257:AAF8PArwpOuNT2XDVO5TRjFEkuIbgxeJzPE"
+BOT_TOKEN = "5586017257:AAF8PAr
 
 # Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-opt>
-MONGO_DB_URI = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://ngodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 # SUDO USERS
-SUDO_USER = 1930645496  # Input type must be interger
+SUDO_USER = 1930  # Input type must be interger
 
 
