35fdbbec76f40e71c5578387662132a473b75d28
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-08-15 16:17:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiAFKBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiAFKBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 from os import getenv
 
@@ -15,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
+API_ID = 18983092
+API_HASH ="a6005a70e88369b4fb08b8350ebbdd35"
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "5586017257:AAF8PArwpOuNT2XDVO5TRjFEkuIbgxeJzPE"
 
-# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-opt>
+MONGO_DB_URI = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 
 # SUDO USERS
-SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
-)  # Input type must be interger
+SUDO_USER = 1930645496  # Input type must be interger
+
+
